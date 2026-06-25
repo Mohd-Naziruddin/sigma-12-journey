6779fe6bd4ec5aab3744a166ea201a14ff9cda0b
MOHD NAZIRUDDIN <mdnaziruddin833@gmail.com>
2026-06-25 09:38:42 +0530

    Add files via upload
    Add JS variables and data types practice

---
+++ variables & data types-JS/practiceset1,2.js
@@ -0,0 +1,20 @@
+// Q1 Create a const object called "product" to store information shown in the picture.: name, rating, offer, price
+const product = {
+    title : "Ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+console.log(product)
+
+// Q2-Create a const object called "profile" to store info shown:username, posts,followers,name,bio
+const profile={
+    username: "shradhakhapra",
+    isfollow: false,
+    postsMade:195,
+    followers:569+"k",
+    following:4,
+    name: "Shradha Khapra",
+    bio: "Apna College| ex-Microsoft,DRDO"
+};
+console.log(profile)
